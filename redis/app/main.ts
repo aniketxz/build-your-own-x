48493016a55be838bf78e5cd5c0ca0af41b53787
Aniket Kumar Mahato <aniketkumar0x@gmail.com>
2026-04-01 14:14:03 +0530

    redis: responding to PING

--- redis/app/main.ts
+++ redis/app/main.ts
@@ -1,11 +1,16 @@
 import * as net from "net";
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
-// Uncomment the code below to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
-	connection.write(`+PONG\r\n`);
+	connection.on("data", (data: Buffer) => {
+		const request = data.toString().trim();
+
+		if (!request.includes("PING")) {
+			return connection.end();
+		}
+
+		const response = "+PONG\r\n";
+		connection.write(response);
+	});
 });
 
 server.listen(6379, "127.0.0.1");
